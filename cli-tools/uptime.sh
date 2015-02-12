b7756e194af5514b850c6c185bf021cee9fec7d3
Inndy <Inndy.mail@gmail.com>
2015-02-12 17:01:34 +0800

    Update uptime.sh

--- cli-tools/uptime.sh
+++ cli-tools/uptime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uptime | awk '{ if ($4 == "days," || $4 == "day,") { print $3 "d " $5 } else { print $3 } }' | tr -d ','
+uptime | awk '{ if ($4 == "days," || $4 == "day," || $4 == "day" || $4 == "days") { print $3 "d " $5 } else { print $3 } }' | tr -d ','
