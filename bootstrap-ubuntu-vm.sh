688ca0b710ef8015cd464285d0f9a32b82cac8ab
Inndy <inndy.mail@gmail.com>
2015-06-25 14:30:42 +0800

    bootstrap: install python package with --upgrade option

--- bootstrap-ubuntu-vm.sh
+++ bootstrap-ubuntu-vm.sh
@@ -20,7 +20,7 @@ ICAgIHxffCAgICAK" | base64 --decode
         apt-get upgrade -y &&
         apt-get install -y git-core tmux vim build-essential g++ python3 python3-dev python3-pip &&
         pip3 install --upgrade pip &&
-        pip3 install requests beautifulsoup4 &&
+        pip3 install --upgrade requests beautifulsoup4 &&
         apt-get install -y $EXTRA_PACKAGE"
 
     git clone https://github.com/inndy/dotfiles ~/.dotfiles
