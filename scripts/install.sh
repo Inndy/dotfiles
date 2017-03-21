719cc404f578d65361eb8ba9ba071fef37fba391
Inndy <inndy.tw@gmail.com>
2017-03-21 21:01:13 +0800

    Update install script and add peda

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,31 +3,27 @@
 echo ""
 echo "  +------------------------------------------------+"
 echo "  |                                                |\\"
-echo "  |    Inndy's config file install script v0.3.0  | \\"
+echo "  |    Inndy's config file install script v0.4.0  | \\"
 echo "  |                                                | |"
 echo "  +------------------------------------------------+ |"
 echo "   \\________________________________________________\\|"
 echo ""
 
-if [ -f ~/.bashrc ]; then
-    echo -ne "~/.bashrc found, do you want to append to it?"
-    read append_to_bashrc
-    if [ "$append_to_bashrc" = "y" -o "$append_to_bashrc" = "Y" ]; then
-        echo "source $HOME/.dotfiles/bashrc" >> ~/.bashrc
-    fi
-fi
+echo "source $HOME/.dotfiles/bashrc" >> ~/.bashrc
+echo "source $HOME/.dotfiles/peda/peda.py" >> ~/.gdbinit
 
-for file in ctags gitignore_global tmux.conf vim vimrc nvim nvimrc inputrc bashrc
+for file in tmux.conf vim vimrc inputrc bashrc gitconfig
 do
-	if [ "$(whoami)" = "inndy" -a "$file" = "gitconfig" ]; then
+	if [ "$(whoami)" != "inndy" -a "$file" = "gitconfig" ]; then
 		continue
 	fi
     ln -s .dotfiles/$file ~/.$file
 done
 
 
-echo "Install cli-tools..."
+echo "Init submodules..."
 git submodule update --init cli-tools
+git submodule update --init peda
 git submodule status
 
 echo<<__LOCAL_RC_FILE__ >> ~/.bashrc.local
