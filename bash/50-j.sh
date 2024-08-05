585448d36fc4d28b95730e21c02dae5c43b5da74
Inndy <inndy.tw@gmail.com>
2024-08-05 17:32:18 +0800

    [bash] Fix `j` plugin behavior

--- bash/50-j.sh
+++ bash/50-j.sh
@@ -3,8 +3,8 @@ _fzf="$(which fzf)"
 if [ -n "$_fzf" -a -x "$_fzf" ]
 then
 	j() {
-		[ -n "$1" ] && p="$1" || p="$(find . -maxdepth 3 -type d \( -name node_modules -o -name .git \) -prune -o -name '*' -type d   -print | fzf)"
-		cd "$p"
+		[ -n "$1" ] && p="$1" || p="$(find . -maxdepth 3 -type d \( -name node_modules -o -name .git \) -prune -o -name '*' -type d -print 2>&- | fzf)"
+		[ $? -eq 0 ] && cd "$p"
 	}
 
 	bind '"\C-j":"j\r"'
