8a56fbfae93b0364447015bbb47aa33e5dd342cf
Inndy <inndy.tw@gmail.com>
2024-12-02 12:14:14 +0800

    [bash] Disable c-j mapping

--- bash/50-j.sh
+++ bash/50-j.sh
@@ -6,8 +6,6 @@ then
 		[ -n "$1" ] && local p="$1" || local p="$(find . -maxdepth 3 -type d \( -name node_modules -o -name .git \) -prune -o -name '*' -type d -print 2>&- | fzf)"
 		[ $? -eq 0 ] && cd "$p"
 	}
-
-	bind '"\C-j":"j\r"'
 fi
 
 # vim: ft=bash
