ed9299ce955216d9a027336e4a1dde03ba6d3287
Inndy <inndy.tw@gmail.com>
2024-12-02 12:54:24 +0800

    [bash] Use PROMPT_COMMAND to rewrite PS1 plugin

--- bash/20-status-line.sh
+++ bash/20-status-line.sh
@@ -1,7 +1,3 @@
-parse_git_branch() {
-    git branch --no-color 2>&- | awk '{ if ($1 == "*" ) { print substr($0, 3) } }'
-}
-
 disable_git_branch_status() {
 	DISABLE_GIT_PARSING=1
 }
@@ -10,14 +6,16 @@ enable_git_branch_status() {
 	DISABLE_GIT_PARSING=
 }
 
-print_git_branch() {
+__print_git_branch() {
 	if [ -n "$DISABLE_GIT_PARSING" ]
 	then
 		echo -ne "\e[01;38;5;242m (GIT INFO OFF)${reset_color}"
 		return
 	fi
 
-    local branch="$(parse_git_branch)"
+    local branch="$(git branch --no-color 2>&- | awk '{ if ($1 == "*" ) { print substr($0, 3) } }')"
+    [ -z "$branch" ] && return
+
 	if [ -z "$(git status --short 2>&1)" ]
 	then
 		# git repo is clean
@@ -26,26 +24,28 @@ print_git_branch() {
 		local git_status_color="${fg_orange}"
 	fi
 
-    [ -z "$branch" ] && return
-
 	echo -ne " ${git_status_color}(${branch})${reset_color}"
 }
 
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-print_virtualenv() {
-	[ -z "$VIRTUAL_ENV" ] && return
-	venv_name=$(basename "$VIRTUAL_ENV")
-	echo -ne " ${fg_yellow}(venv:${venv_name})${reset_color}"
-}
+#export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 # [ $DATE ] $USER @ $HOST $PWD $GIT_STATUS $VIRTUALENV_STATUS
 # $
 
-PS1=\
-"\[${fg_yellow}\][ \$(date '+%m/%d %H:%M:%S') ] "\
-"\[${fg_magenta}\]\u \[${reset_color}\]@ \[${fg_green}\]\h "\
-"\[${fg_lightblue}\]\w"\
-"\$(print_git_branch)"\
-"\$(print_virtualenv)"\
-"\n"\
-"\[${vt100_bold}\]\$\[${reset_color}\] "
+function __prompt_command() {
+	local last_program_status="$?"
+	PS1="\[${fg_yellow}\][ \$(date '+%m/%d %H:%M:%S') ] "
+	PS1+="\[${fg_magenta}\]\u \[${reset_color}\]@ \[${fg_green}\]\h "
+	PS1+="\[${fg_lightblue}\]\w"
+	[ $last_program_status -ne 0 ] && PS1+=" \[${fg_pink}\](exit=$last_program_status)"
+	__ps1_git_info="$(__print_git_branch)"
+	PS1+="\${__ps1_git_info}"
+	[ -n "$VIRTUAL_ENV" ] && {
+		__ps1_venv_name=$(basename "$VIRTUAL_ENV")
+		PS1+=" ${fg_yellow}(venv:\${__ps1_venv_name})${reset_color}"
+	}
+	PS1+="\n"
+	PS1+="\[${vt100_bold}\]\$\[${reset_color}\] "
+}
+
+PROMPT_COMMAND="__prompt_command${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
