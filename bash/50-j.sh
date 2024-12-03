1b852b71eacbde5cab5306f48f1ee9b716571f5d
Inndy <inndy.tw@gmail.com>
2024-12-03 16:50:36 +0800

    [bash] Update `j` plugin to include ancestor folders

--- bash/50-j.sh
+++ bash/50-j.sh
@@ -2,8 +2,16 @@ _fzf="$(which fzf)"
 
 if [ -n "$_fzf" -a -x "$_fzf" ]
 then
+	__bash_plugin_j_list() {
+		echo ..
+		echo ../..
+		echo ../../..
+		echo ../../../..
+		echo ../../../../..
+		find . -maxdepth 4 -type d \( -name node_modules -o -name .git \) -prune -o -name '*' -type d -print 2>&-
+	}
 	j() {
-		[ -n "$1" ] && local p="$1" || local p="$(find . -maxdepth 3 -type d \( -name node_modules -o -name .git \) -prune -o -name '*' -type d -print 2>&- | fzf)"
+		[ -n "$1" ] && local p="$1" || local p="$(__bash_plugin_j_list | fzf)"
 		[ $? -eq 0 ] && cd "$p"
 	}
 fi
