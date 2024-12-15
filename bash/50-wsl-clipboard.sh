7e166d94aaf53c75384f82ee3ef47cca14da406f
Inndy <inndy.tw@gmail.com>
2024-12-16 00:41:37 +0800

    [bash] Use same command to copy/paste on macOS

--- bash/50-wsl-clipboard.sh
+++ bash/50-wsl-clipboard.sh
@@ -1,11 +1,11 @@
 if [ -n "$WSL_DISTRO_NAME" -o -n "$WSLENV" ]
 then
-	function paste()
+	function pbpaste()
 	{
 		powershell.exe -Command '[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8; Get-Clipboard'
 	}
 
-	function copy()
+	function pbcopy()
 	{
 		powershell.exe -Command 'Set-Clipboard ([Console]::In.ReadToEnd())'
 	}
