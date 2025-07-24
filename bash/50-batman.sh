4aa7f1700a2525e48f53ef26151ad305b4bcf5ae
Inndy <inndy.tw@gmail.com>
2025-07-24 15:28:09 +0800

    [bash] Help batman locate himself in Ubuntu/Debian

--- bash/50-batman.sh
+++ bash/50-batman.sh
@@ -1,11 +1,14 @@
-if [[ $- == *i* && $(command -v bat) ]]; then
-	function man() {
-		if [[ -t 1 ]]; then
-			MANWIDTH=$(($(tput cols) - 9)) command man "$@" | col -b | bat --language=man
-		else
-			command man "$@"
-		fi
-	}
+if [[ $- == *i* ]]; then
+    function man() {
+        local pager
+        pager=$(command -v bat 2>/dev/null || command -v batcat 2>/dev/null)
+
+        if [[ -t 1 && -n "$pager" ]]; then
+            MANWIDTH=$(($(tput cols) - 9)) command man "$@" | col -b | "$pager" --language=man
+        else
+            command man "$@"
+        fi
+    }
 fi
 
 # vim: ft=bash
